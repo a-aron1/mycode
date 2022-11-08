71f39168a001c00505b5676c64e2603ddbf109b2
a-aron1 <syrus1422@yahoo.com>
2022-11-08 17:13:59 +0000

    update files again

--- lab_input/input.py
+++ lab_input/input.py
@@ -4,3 +4,5 @@ name = input("What is your name?")
 date = input("What day is it?")
 
 print("Hello, " + name + "! Happy " + date + "!!!")
+
+ """ can also be written print(f"Hello, {name}! Happy {day}!!!)"""
