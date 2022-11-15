b9a34a39854a0356c4c9ac8b316616f18b994463
a-aron1 <syrus1422@yahoo.com>
2022-11-15 20:25:06 +0000

    reviewing how to manipulate JSON data with python

---
+++ pandas/pandastest.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+pet1 = {"name": "Misty", "age":12, "species":"cat"}
+pet2 = {"name": "Peach", "age":1, "species":"cat"}
+pet3 = {"name": "Pepper", "age":5, "species":"cat"}
+pet4 = {"name": "Bones", "age":8, "species":"cdog"}
+pet5 = {"name": "Bernie", "age":3, "species":"hamster"}
+pet6 = {"name": "Greg", "species":"fish"}
+
+
+
