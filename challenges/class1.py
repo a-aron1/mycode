99140e0315c6497cda8c36b9a39ee59da599b121
a-aron1 <syrus1422@yahoo.com>
2022-11-15 17:08:28 +0000

    Learning about clases

---
+++ challenges/class1.py
@@ -0,0 +1,11 @@
+class Cat:
+    def __init__(self, name):
+        return None
+    def speak(self):
+        return 'meow'
+
+mycat=Cat("Paco Wong")
+print(mycat)
+print(type(mycat))
+print(dir(mycat))
+print(mycat.speak())
